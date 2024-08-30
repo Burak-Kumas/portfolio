8fe625c93df019fdf0396f12585bdad9fcc22669
Burak Kumaş <brkkumas@gmail.com>
2024-08-30 17:20:38 +0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: {
-        title: 'Burak Kumaş',
-        description: 'Burak Kumaş - Frontend Developer',
-        url: 'https://burakkumas.com',
-        base: '/',
-    },
+  site: "https://burakkumas.com",
 });
